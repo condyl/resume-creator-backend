cff32cf2810407eb6da4bcaf15db41a9417e2d2c
condyl <Condyl678@gmail.com>
2024-12-30 15:06:02 -0500

    add pam requested changes

--- src/resumeGenerator/projects.js
+++ src/resumeGenerator/projects.js
@@ -1,5 +1,11 @@
 const { escapeLatex } = require('./escapeLatex');
 
+const formatDate = (dateString) => {
+    if (dateString === 'Present') return 'Present';
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', { month: 'short', year: 'numeric' });
+}
+
 const generateProjectsSection = (projects) => {
     return projects
         .map(
@@ -9,10 +15,14 @@ const generateProjectsSection = (projects) => {
                 if (project.githubUrl) links.push(`\\href{${escapeLatex(project.githubUrl)}}{\\underline{GitHub}}`);
                 const linkText = links.length > 0 ? `${links.join(' $|$ ')}` : '';
                 const techText = project.technologies ? ` $|$ \\emph{${escapeLatex(project.technologies)}}` : '';
+                const formattedDate = project.showDate ? (project.endDate === 'Present' ? 
+                    `${formatDate(project.startDate)} - Present` : 
+                    `${formatDate(project.startDate)} - ${formatDate(project.endDate)}`) : '';
+                const dateText = formattedDate ? (links.length > 0 ? ` $|$ ${escapeLatex(formattedDate)}` : ` ${escapeLatex(formattedDate)}`) : '';
 
                 return `
         \\resumeProjectHeading
-          {\\textbf{${escapeLatex(project.name)}}${techText}}{${linkText}}
+          {\\textbf{${escapeLatex(project.name)}}${techText}}{${linkText}${dateText}}
           \\resumeItemListStart
             ${project.details.map((detail) => `\\resumeItem{${escapeLatex(detail)}}`).join("\n")}
             \\resumeItemListEnd
